640da8bf0d7941f2932ea42b4399c53a8d9469fe
Ivan Jovanovic <mrtanguero@gmail.com>
2021-05-29 14:17:31 +0200

    Fix button positions on create new user and edit document views

--- public/js/documents/create.js
+++ public/js/documents/create.js
@@ -11,7 +11,10 @@ const fillSerialNumbers = async () => {
     markup = "";
     const data = await fetchSerials(equipmentSelect.value);
     data.forEach((serial) => {
-        markup += `<option value="${serial.serial_number}">${serial.serial_number}</option>`;
+        markup += `
+            <option value="${serial.serial_number}">
+                ${serial.serial_number}
+            </option>`;
     });
     serialNumber.innerHTML = markup;
 };
